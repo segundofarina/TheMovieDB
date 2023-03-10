f0c4be10e6db51f4955fc3c4c22898c5ef477bbe
Segundo Farina <segundo.farina@madmobile.com>
2023-03-10 12:01:25 -0300

    Initial Commit

---
+++ TheMovieDB/TheMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieDBApp.swift
+//  TheMovieDB
+//
+//  Created by Segundo Fariña on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
