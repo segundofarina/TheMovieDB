3cd032f6c91a07927a45011bb10165867dc291ad
Segundo Farina <segundo.farina@madmobile.com>
2023-03-16 11:21:16 -0300

    Add color assets

---
+++ TheMovieDB/Extensions/Color+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Color+Extensions.swift
+//  TheMovieDB
+//
+//  Created by Segundo Fariña on 10/03/2023.
+//
+
+import SwiftUI
+
+extension Color {
+  static var BackgroundListView: Color {
+    Color(UIColor(named: "BackgroundListView")!)
+  }
+  static var iceBlue: Color {
+    Color(UIColor(named: "IceBlue")!)
+  }
+  
+  
+}
